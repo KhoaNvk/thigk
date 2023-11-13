38fcd92796d2e8c9f53b0b4a14cf620dafb3c088
KhoaNvk <khoasama998@gmail.com>
2023-11-13 15:56:49 +0700

    chinh phuong

--- source.cpp
+++ source.cpp
@@ -1,9 +1,13 @@
 using namespace std;
-
+int chinhphuong(int x)
+{
+	return x;
+}
 int main(){
 	int a;
 	cou<<"nhap so nguyen";
 	cin>>a;
 	cout<<"so nguyen la"<<a<<endl;
+	cout<<chinhphuong(a);
 	
 }
